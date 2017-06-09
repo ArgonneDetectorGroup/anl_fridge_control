2a7d022117dcca8e6ff54ee7c63623315e062ab9
Lauren Saunders <lausaunders@gmail.com>
2017-06-09 08:48:41 -0500

    hwm-related revisions

--- control/basic_functions.py
+++ control/basic_functions.py
@@ -5,72 +5,23 @@
 #
 # LJS 2016-10-14
 
-#from matplotlib.pylab import switch_backend
-#switch_backend('Agg')
-import serial
-import pydfmux
-import os,time
 import datetime
 import numpy as np
-from pydfmux.core.utils.conv_functs import build_hwm_query
-#from matplotlib.pylab import switch_backend
-#switch_backend('Agg')
-
 import os,time
+import pydfmux.spt3g.northern_tuning_script as nts
 import sys
 sys.path.append('/home/spt3g/')
 
 import he10_fridge_control.control.gettemp as gt
-
-import numpy as np
 import anl_fridge_control.serial_connections as sc
-
-###############################################################################
-############## Edit this first part to do what you want it to do ##############
-###############################################################################
-
-# Choose a hardware map and IceBoard
-
-hwm_dir='/home/spt3g/hardware_maps/hwm_anl_20170306_W169_and_Rboards/hwm_anl_proto.yml'
-board='0137'
-
-hwm=pydfmux.load_session(open(hwm_dir))['hardware_map']
-ds=hwm.query(pydfmux.Dfmux)
-sqcbs=hwm.query(pydfmux.SQUIDController)
-squids=hwm.query(pydfmux.SQUID)
-bolos = hwm.query(pydfmux.Bolometer)
-rm=hwm.query(pydfmux.ReadoutModule)
-d=ds[0]
-
-# Choose which modules to use when overbiasing/dropping bolometers
-
-gbolos=[]
-for bb in bolos.all():
-    if (bb.iceboard != None) and (bb.iceboard.serial !=None):
-        if bb.iceboard.serial==board and bb.readout_channel.mezzanine.mezzanine==1 and (bb.readout_channel.module.module==2):
-            gbolos.append(bb)
-        if bb.iceboard.serial==board and bb.readout_channel.mezzanine.mezzanine==1 and (bb.readout_channel.module.module==3):
-            gbolos.append(bb)
-        if bb.iceboard.serial==board and bb.readout_channel.mezzanine.mezzanine==1 and (bb.readout_channel.module.module==4):
-
-            gbolos.append(bb)
-        if bb.iceboard.serial==board and bb.readout_channel.mezzanine.mezzanine==2 and (bb.readout_channel.module.module==1):
-            gbolos.append(bb)
-        if bb.iceboard.serial==board and bb.readout_channel.mezzanine.mezzanine==2 and (bb.readout_channel.module.module==2):
-            gbolos.append(bb)
-        if bb.iceboard.serial==board and bb.readout_channel.mezzanine.mezzanine==2 and (bb.readout_channel.module.module==3):
-            gbolos.append(bb)
-        if bb.iceboard.serial==board and bb.readout_channel.mezzanine.mezzanine==2 and (bb.readout_channel.module.module==4):
-            gbolos.append(bb)
-
 
 ###############################################################################
 ##### Items below do not need to be changed after setting for your fridge #####
 ###############################################################################
 
-def start_of_day(logfile, set_squid_feedback=False, set_gain=False):
+def start_of_day(logfile, heating_type, set_squid_feedback=False, set_gain=False):
 	'''
-	Starts things for you by heating the UC head (using PID and pump), initializing the board, tuning squids, and taking a rawdump.
+	Starts things for you by heating the UC Stage, initializing the board, tuning squids, and taking a rawdump.
 	'''
 	# set voltages to 0, let the switches cool
 	print 'Setting the switches to zero to cool.'
@@ -82,89 +33,101 @@ def start_of_day(logfile, set_squid_feedback=False, set_gain=False):
 	sc.He3UCp.set_voltage(0)
 	sc.He3UCs.set_voltage(0)
 
-	gt.gettemp(logfile, 'He3 IC Switch'); gt.gettemp(logfile, 'He3 UC Switch')
+    gt.gettemp(logfile, 'He3 IC Switch'); gt.gettemp(logfile, 'He3 UC Switch')
 	while float(gt.gettemp(logfile, 'He3 UC Switch'))>1:
 		while float(gt.gettemp(logfile, 'He3 IC Switch'))>1:
 			gt.gettemp(logfile, 'He3 UC Switch'); gt.gettemp(logfile, 'He3 IC Switch')
 			time.sleep(10)
 
-	# set UC pump voltage and start the heater to get above Tc
-	print 'Setting ultra pump to 3.00 V.'
-	sc.He3UCp.set_voltage(3.00)
-	time.sleep(60)
-	print 'Setting the heater to 500 mK at 1.5 mW.'
-	sc.ChaseLS.set_PID_temp(1,0.500)
-	time.sleep(1)
-	sc.ChaseLS.set_heater_range(2)
-	time.sleep(1)
-	gt.gettemp(logfile, 'UC Head')
-	while float(gt.gettemp(logfile, 'UC Head'))<0.500:
-		time.sleep(20)
-		gt.gettemp(logfile, 'UC Head')
-	time.sleep(1)
-	print 'Setting the heater to 650 mK at 15 mW.'
-	sc.ChaseLS.set_PID_temp(1, 0.650)
-	time.sleep(1)
-	sc.ChaseLS.set_heater_range(3)
-	time.sleep(1)
+    if heating_type=='PID':
+    	# set UC pump voltage and start the heater to get above Tc
+    	print 'Setting ultra pump to 3.00 V.'
+    	sc.He3UCp.set_voltage(3.00)
+    	time.sleep(60)
+    	print 'Setting the heater to 500 mK at 1.5 mW.'
+    	sc.ChaseLS.set_PID_temp(1,0.500)
+    	time.sleep(1)
+    	sc.ChaseLS.set_heater_range(2)
+    	time.sleep(1)
+    	gt.gettemp(logfile, 'UC Head')
+    	while float(gt.gettemp(logfile, 'UC Head'))<0.500:
+    		time.sleep(20)
+    		gt.gettemp(logfile, 'UC Head')
+    	time.sleep(1)
+    	print 'Setting the heater to 650 mK at 15 mW.'
+    	sc.ChaseLS.set_PID_temp(1, 0.650)
+    	time.sleep(1)
+    	sc.ChaseLS.set_heater_range(3)
+    	time.sleep(1)
 
-	print 'Setting the ultra pump to 1.50 V'
-	sc.He3UCp.set_voltage(1.00)
-	while float(gt.gettemp(logfile, 'UC Head'))<0.650:
-		time.sleep(20)
-		gt.gettemp(logfile, 'UC Head')
+    	print 'Setting the ultra pump to 1.50 V'
+    	sc.He3UCp.set_voltage(1.00)
+    	while float(gt.gettemp(logfile, 'UC Head'))<0.650:
+    		time.sleep(20)
+    		gt.gettemp(logfile, 'UC Head')
 
-	sc.He3UCp.set_voltage(1.50)
-	print 'Waiting...'
-	time.sleep(300)
+    	sc.He3UCp.set_voltage(1.50)
+    	print 'Waiting...'
+    	time.sleep(300)
+
+    elif heating_type=='pumps':
+        print 'Setting the He3 UC pump to 3.00 V'
+        sc.He3UCp.set_voltage(3.00)
+        print 'Setting the He3 IC pump to 3.00 V'
+        sc.He3ICp.set_voltage(3.00)
+
+        print 'Waiting for IC Head temperature to rise'
+        while float(gt.gettemp(logfile, 'IC Head'))<0.650:
+            time.sleep(20)
+        print 'Setting He3 IC pump to 0.50 V'
+        sc.He3ICp.set_voltage(0.50)
+
+        print 'Waiting for UC Head temperature to rise'
+        while float(gt.gettemp(logfile, 'UC Head'))<0.650:
+            time.sleep(20)
+        print 'Setting He3 UC pump to 1.80 V'
+        sc.He3UCp.set_voltage(1.80)
+
+        print 'Waiting 5 minutes...'
+        time.sleep(300)
 
 	# initialize the board
 	print 'Initializing the board.'
 	try:
-		ds.initialize_iceboard(use_test_IRIG=True)
-		sqcbs.initialize_squidcontroller()
-		time.sleep(5)
-		ds.set_fir_stage(6)
-		ds.align_sampling()
+		nts.run_initialize()
 	except URLError:
 		print 'Could not initialize because the power is not on!  Waiting...'
 		initboard = raw_input("Type 'done' when the power is set to 17 V.    ")
 		if initboard == 'done':
-			ds.initialize_iceboard(use_test_IRIG=True)
-			sqcbs.initialize_squidcontroller()
-			time.sleep(5)
-			ds.set_fir_stage(6)
-			ds.align_sampling()
+			nts.run_initialize()
 
 	# zero combs
-	ds.clear_all()
-	ds.clear_dan()
+	print 'Zeroing combs'
+    nts.run_do_zero_combs()
 
 	# heat the squids
 	print 'Heating SQUIDs.'
-	sqcbs.heat_squids(heat_time_sec=15,heat_voltage=4.0,wait_time_sec=1200)
+	nts.run_heat_squids()
 
 	# tune the squids
 	print 'Tuning SQUIDs.'
-	sq_results=squids.tune_squid(squid_bias_reference=[1.6],squid_bias_start=0.8,flux_start=0,flux_stop=2.0,minap2p=0.05)
+	nts.run_tune_squids()
 
 	# squid feedback (optional)
 	if set_squid_feedback:
 		print 'Setting SQUID feedback.'
-		for squid in squids.all():
-			squid.module.set_squid_feedback('SQUID_LOWPASS')
+        nts.run_enable_feedback()
 
 	# set gain (optional)
 	if set_gain:
 		print 'Setting gain.'
-		rm.set_mezzanine_gain(15,'CARRIER')
-		rm.set_mezzanine_gain(15,'NULLER')
+		nts.run_set_gain()
 
 	# take a rawdump
 	print 'Taking a rawdump.'
-	results=rm.take_rawdump(num_samples=50000)
+	nts.run_take_rawdump()
 
-	print 'Startup is complete.  Start whatever you want to do today!'
+	print 'Startup is complete!'
 
 
 def zero_everything():
@@ -214,125 +177,125 @@ def finish_cycle(hex_filename):
 
 	time.sleep(3600)
 
-	print 'Cycle is complete!'
+	print 'The hex_watcher_3000 has done its duty.'
 	print datetime.datetime.now()
 
-def autocycle(logfile, start=False):
-	'''
-	This is a function to run a fridge cycle.  Note that it takes 6+ hours to run.
-	'''
-	try:
-		print "Turning off mezzanines."
-		ds.set_mezzanine_power(False,1)
-		ds.set_mezzanine_power(False,2)
-		time.sleep(60)
-
-		print "Mezzanines off, ready to go."
-
-		sc.He4p.remote_set()
-		sc.He3ICp.remote_set()
-		sc.He3UCp.remote_set()
-
-		print 'Setting the heater, switches, and pumps to 0.'
-
-		zero_everything()
-
-		print 'Waiting for switches to cool.'
-
-		while float(gt.gettemp(logfile, 'He3 IC Switch'))>2.00:
-			time.sleep(10)
-		while float(gt.gettemp(logfile, 'He3 UC Switch'))>2.00:
-			time.sleep(10)
-		while float(gt.gettemp(logfile, 'He4 IC Switch'))>4.00:
-			time.sleep(10)
-		print 'Switches are cool, beginning heating.'
-
-		print ''
-
-		print 'Turning He4 pump to 12.00 V.'
-		sc.He4p.set_voltage(12.00)
-
-		print 'Turning inter pump to 9.60 V.'
-		sc.He3ICp.set_voltage(9.60)
-
-		print 'Turning ultra pump to 6.30 V.'
-		sc.He3UCp.set_voltage(6.30)
-
-		print ''
-
-		print 'Waiting 30 minutes for pumps to heat.'
-		t=0
-		while t<1800:
-			time.sleep(60)
-			t=t+60
-			minutes=str(t/60)
-			print ('%s minutes elapsed so far.' % minutes)
-
-		print 'Turning He4 pump to 9.50 V.'
-		sc.He4p.set_voltage(9.50)
-
-		print 'Turning IC pump to 5.10 V.'
-		sc.He3ICp.set_voltage(5.10)
-
-		print 'Turning UC pump to 5.30 V.'
-		sc.He3UCp.set_voltage(5.30)
-
-		print 'Waiting 20 minutes.'
-		t=0
-		while t<1200:
-			time.sleep(60)
-			t=t+60
-			minutes=str(t/60)
-			print ('%s minutes elapsed so far.' % minutes)
-
-		print 'Turning He4 pump to 5.0 V.'
-		sc.He4p.set_voltage(5.0)
-
-		print 'Waiting for He4 pump temperature to reach 43 K.'
-		while float(gt.gettemp(logfile, 'He4 IC Pump'))<43:
-			time.sleep(10)
-
-		print 'Turning He4 pump to 3.0 V.'
-		sc.He4p.set_voltage(3.00)
-
-		print 'Waiting 5 minutes.'
-		time.sleep(300)
-
-		print 'Setting He4 pump to 0.0 V.'
-		sc.He4p.set_voltage(0.00)
-
-		print 'Sleeping for 3 minutes.'
-		time.sleep(180)
-
-		print 'Setting He4 switch to 4.00 V.'
-		sc.He4s.set_voltage(4.00)
-
-		print 'Waiting for He3 IC Pump temperatures to change.'
-		while float(gt.gettemp(logfile, 'He3 IC Pump'))<52:
-			time.sleep(10)
-
-		print 'Setting inter pump to 3.0 V.'
-		sc.He3ICp.set_voltage(3.00)
-
-		print 'Waiting for He3 UC Pump temperature to change.'
-		while float(gt.gettemp(logfile, 'He3 UC Pump'))<52:
-			time.sleep(10)
-
-		print 'Setting ultra pump to 5.00 V.'
-		sc.He3UCp.set_voltage(5.00)
-
-		finish_cycle(logfile)
-
-		if start==True:
-			start_of_day()
-		else:
-			print 'Cycle is complete.'
-			return
-
-	except:
-		print 'Crashed!'
-		print datetime.datetime.now()
-		zero_everything()
+# def autocycle(logfile, start=False):
+# 	'''
+# 	This is a function to run a fridge cycle.  Note that it takes 6+ hours to run.
+# 	'''
+# 	try:
+# 		print "Turning off mezzanines."
+# 		ds.set_mezzanine_power(False,1)
+# 		ds.set_mezzanine_power(False,2)
+# 		time.sleep(60)
+#
+# 		print "Mezzanines off, ready to go."
+#
+# 		sc.He4p.remote_set()
+# 		sc.He3ICp.remote_set()
+# 		sc.He3UCp.remote_set()
+#
+# 		print 'Setting the heater, switches, and pumps to 0.'
+#
+# 		zero_everything()
+#
+# 		print 'Waiting for switches to cool.'
+#
+# 		while float(gt.gettemp(logfile, 'He3 IC Switch'))>2.00:
+# 			time.sleep(10)
+# 		while float(gt.gettemp(logfile, 'He3 UC Switch'))>2.00:
+# 			time.sleep(10)
+# 		while float(gt.gettemp(logfile, 'He4 IC Switch'))>4.00:
+# 			time.sleep(10)
+# 		print 'Switches are cool, beginning heating.'
+#
+# 		print ''
+#
+# 		print 'Turning He4 pump to 12.00 V.'
+# 		sc.He4p.set_voltage(12.00)
+#
+# 		print 'Turning inter pump to 9.60 V.'
+# 		sc.He3ICp.set_voltage(9.60)
+#
+# 		print 'Turning ultra pump to 6.30 V.'
+# 		sc.He3UCp.set_voltage(6.30)
+#
+# 		print ''
+#
+# 		print 'Waiting 30 minutes for pumps to heat.'
+# 		t=0
+# 		while t<1800:
+# 			time.sleep(60)
+# 			t=t+60
+# 			minutes=str(t/60)
+# 			print ('%s minutes elapsed so far.' % minutes)
+#
+# 		print 'Turning He4 pump to 9.50 V.'
+# 		sc.He4p.set_voltage(9.50)
+#
+# 		print 'Turning IC pump to 5.10 V.'
+# 		sc.He3ICp.set_voltage(5.10)
+#
+# 		print 'Turning UC pump to 5.30 V.'
+# 		sc.He3UCp.set_voltage(5.30)
+#
+# 		print 'Waiting 20 minutes.'
+# 		t=0
+# 		while t<1200:
+# 			time.sleep(60)
+# 			t=t+60
+# 			minutes=str(t/60)
+# 			print ('%s minutes elapsed so far.' % minutes)
+#
+# 		print 'Turning He4 pump to 5.0 V.'
+# 		sc.He4p.set_voltage(5.0)
+#
+# 		print 'Waiting for He4 pump temperature to reach 43 K.'
+# 		while float(gt.gettemp(logfile, 'He4 IC Pump'))<43:
+# 			time.sleep(10)
+#
+# 		print 'Turning He4 pump to 3.0 V.'
+# 		sc.He4p.set_voltage(3.00)
+#
+# 		print 'Waiting 5 minutes.'
+# 		time.sleep(300)
+#
+# 		print 'Setting He4 pump to 0.0 V.'
+# 		sc.He4p.set_voltage(0.00)
+#
+# 		print 'Sleeping for 3 minutes.'
+# 		time.sleep(180)
+#
+# 		print 'Setting He4 switch to 4.00 V.'
+# 		sc.He4s.set_voltage(4.00)
+#
+# 		print 'Waiting for He3 IC Pump temperatures to change.'
+# 		while float(gt.gettemp(logfile, 'He3 IC Pump'))<52:
+# 			time.sleep(10)
+#
+# 		print 'Setting inter pump to 3.0 V.'
+# 		sc.He3ICp.set_voltage(3.00)
+#
+# 		print 'Waiting for He3 UC Pump temperature to change.'
+# 		while float(gt.gettemp(logfile, 'He3 UC Pump'))<52:
+# 			time.sleep(10)
+#
+# 		print 'Setting ultra pump to 5.00 V.'
+# 		sc.He3UCp.set_voltage(5.00)
+#
+# 		finish_cycle(logfile)
+#
+# 		if start==True:
+# 			start_of_day()
+# 		else:
+# 			print 'Cycle is complete.'
+# 			return
+#
+# 	except:
+# 		print 'Crashed!'
+# 		print datetime.datetime.now()
+# 		zero_everything()
 
 #def first_cycle():
 	# set the power supplies to remote mode
